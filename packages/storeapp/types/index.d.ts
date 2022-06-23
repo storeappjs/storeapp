4fa17a716330d890c7b55c59d2a9bee25dadba7e
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-06-23 18:03:30 +0900

    add type

---
+++ packages/storeapp/types/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Install Application
+ * see - https://storeappjs.netlify.app/documents/javascript/api#install
+ * @param appName Application Name
+ */
+export default function installApp(appName: string): Promise<void>;
+/**
+ * Run Application
+ * see - https://storeappjs.netlify.app/documents/javascript/api#run
+ * @param appName Application Name
+ */
+export default function runApp(appName: string): void;
